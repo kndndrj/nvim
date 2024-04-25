d6741e89776c262e9325a85e4d96e98063888956
Andrej Kenda <andrej.baca14@gmail.com>
2024-04-25 12:49:47 +0200

    refactor(dbee): split into multiple files

--- lua/plugins/dbee/layout.lua
+++ lua/plugins/dbee/layout.lua
@@ -1,11 +1,6 @@
--------------------------
--- Dbee settings: -------
--------------------------
 local api_ui = require("dbee").api.ui
 local utils = require("dbee.utils")
 local tools = require("dbee.layouts.tools")
-
-local M = {}
 
 -- Slayout is a layout that slays.
 -- I DON'T KNOW WHY I PICKED THIS STUPID NAME, BUT I DID, OKAY!
@@ -174,62 +169,4 @@ function Slayout:close()
   self.is_opened = false
 end
 
---
--- Configuration function
---
-function M.configure(conns)
-  local default = require("dbee.config").default
-
-  -- configure in-memory sources if any
-  local sources
-  if conns and not vim.tbl_isempty(conns) then
-    sources = vim.list_extend({ require("dbee.sources").MemorySource:new(conns) }, default.sources)
-  else
-    sources = default.sources
-  end
-
-  -- add extra mappings to all dbee views
-  local slayout = Slayout:new()
-  local drawer_keymap = {
-    key = "BC",
-    mode = "n",
-    action = function()
-      slayout:open_drawer()
-    end,
-  }
-  local call_log_keymap = {
-    key = "BL",
-    mode = "n",
-    action = function()
-      slayout:open_call_log()
-    end,
-  }
-
-  local expand_mappings = function(defaults)
-    table.insert(defaults, drawer_keymap)
-    table.insert(defaults, call_log_keymap)
-    return defaults
-  end
-
-  -- setup function
-  require("dbee").setup {
-    sources = sources,
-    drawer = {
-      disable_help = true,
-      mappings = expand_mappings(default.drawer.mappings),
-    },
-    editor = {
-      mappings = expand_mappings(default.editor.mappings),
-    },
-    call_log = {
-      mappings = expand_mappings(default.call_log.mappings),
-    },
-    result = {
-      mappings = expand_mappings(default.result.mappings),
-    },
-
-    window_layout = slayout,
-  }
-end
-
-return M
+return Slayout
